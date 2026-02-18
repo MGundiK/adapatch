394d4ffa2a98e100bdd03627232842f8ab3f7bc4
MGundiK <gundic@gmail.com>
2026-02-18 14:08:25 +0100

    Update exp_main.py

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -1,6 +1,6 @@
 from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
-from models import xPatch
+from models import AdaPatch
 from utils.tools import EarlyStopping, adjust_learning_rate, visual
 from utils.metrics import metric
 
@@ -15,41 +15,48 @@ import math
 
 warnings.filterwarnings('ignore')
 
+
 class Exp_Main(Exp_Basic):
     def __init__(self, args):
         super(Exp_Main, self).__init__(args)
+        self._build_arctan_weights()
 
     def _build_model(self):
-        model_dict = {
-            'xPatch': xPatch,
-        }
-        model = model_dict[self.args.model].Model(self.args).float()
-
+        model = AdaPatch.Model(self.args).float()
         if self.args.use_multi_gpu and self.args.use_gpu:
             model = nn.DataParallel(model, device_ids=self.args.device_ids)
         return model
+
+    def _build_arctan_weights(self):
+        """Precompute arctangent loss scaling weights (xPatch innovation)."""
+        ratio = np.array([
+            -1 * math.atan(i + 1) + math.pi / 4 + 1
+            for i in range(self.args.pred_len)
+        ])
+        self.ratio = torch.tensor(ratio, dtype=torch.float32).unsqueeze(-1)
 
     def _get_data(self, flag):
         data_set, data_loader = data_provider(self.args, flag)
         return data_set, data_loader
 
     def _select_optimizer(self):
-        # model_optim = optim.Adam(self.model.parameters(), lr=self.args.learning_rate)
-        model_optim = optim.AdamW(self.model.parameters(), lr=self.args.learning_rate)
-        return model_optim
+        return optim.AdamW(self.model.parameters(), lr=self.args.learning_rate)
 
-    # # MSE criterion
-    # def _select_criterion(self):
-    #     criterion = nn.MSELoss()
-    #     return criterion
-
-    # MSE and MAE criterion
     def _select_criterion(self):
-        mse_criterion = nn.MSELoss()
-        mae_criterion = nn.L1Loss()
-        return mse_criterion, mae_criterion
+        return nn.MSELoss(), nn.L1Loss()
 
-    def vali(self, vali_data, vali_loader, criterion, is_test = True):
+    def _get_ratio(self):
+        return self.ratio.to(self.device)
+
+    def _get_model_attr(self, attr):
+        """Helper for DataParallel compatibility."""
+        if hasattr(self.model, attr):
+            return getattr(self.model, attr)
+        elif hasattr(self.model, 'module'):
+            return getattr(self.model.module, attr)
+        return None
+
+    def vali(self, vali_data, vali_loader, criterion, is_test=True):
         total_loss = []
         self.model.eval()
         with torch.no_grad():
@@ -57,39 +64,22 @@ class Exp_Main(Exp_Basic):
                 batch_x = batch_x.float().to(self.device)
                 batch_y = batch_y.float()
 
-                batch_x_mark = batch_x_mark.float().to(self.device)
-                batch_y_mark = batch_y_mark.float().to(self.device)
-
-                # decoder input
-                dec_inp = torch.zeros_like(batch_y[:, -self.args.pred_len:, :]).float()
-                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
-                # encoder - decoder
                 outputs = self.model(batch_x)
                 f_dim = -1 if self.args.features == 'MS' else 0
                 outputs = outputs[:, -self.args.pred_len:, f_dim:]
                 batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
 
-                # if train, use ratio to scale the prediction
                 if not is_test:
-                    # CARD loss with weight decay
-                    # self.ratio = np.array([max(1/np.sqrt(i+1),0.0) for i in range(self.args.pred_len)])
-
-                    # Arctangent loss with weight decay
-                    self.ratio = np.array([-1 * math.atan(i+1) + math.pi/4 + 1 for i in range(self.args.pred_len)])
-                    self.ratio = torch.tensor(self.ratio).unsqueeze(-1).to('cuda')
-
-                    pred = outputs*self.ratio
-                    true = batch_y*self.ratio
+                    ratio = self._get_ratio()
+                    pred = outputs * ratio
+                    true = batch_y * ratio
                 else:
-                    pred = outputs#.detach().cpu()
-                    true = batch_y#.detach().cpu()
-
-                # pred = outputs.detach().cpu()
-                # true = batch_y.detach().cpu()
+                    pred = outputs
+                    true = batch_y
 
                 loss = criterion(pred, true)
-
                 total_loss.append(loss.item())
+
         total_loss = np.average(total_loss)
         self.model.train()
         return total_loss
@@ -104,81 +94,45 @@ class Exp_Main(Exp_Basic):
             os.makedirs(path)
 
         time_now = time.time()
-
         train_steps = len(train_loader)
         early_stopping = EarlyStopping(patience=self.args.patience, verbose=True)
-
         model_optim = self._select_optimizer()
-        # criterion = self._select_criterion() # For MSE criterion
         mse_criterion, mae_criterion = self._select_criterion()
 
-        # # CARD's cosine learning rate decay with warmup
-        # self.warmup_epochs = self.args.warmup_epochs
+        alpha_history = []
 
-        # def adjust_learning_rate_new(optimizer, epoch, args):
-        #     """Decay the learning rate with half-cycle cosine after warmup"""
-        #     min_lr = 0
-        #     if epoch < self.warmup_epochs:
-        #         lr = self.args.learning_rate * epoch / self.warmup_epochs 
-        #     else:
-        #         lr = min_lr+ (self.args.learning_rate - min_lr) * 0.5 * \
-        #             (1. + math.cos(math.pi * (epoch - self.warmup_epochs) / (self.args.train_epochs - self.warmup_epochs)))
-                
-        #     for param_group in optimizer.param_groups:
-        #         if "lr_scale" in param_group:
-        #             param_group["lr"] = lr * param_group["lr_scale"]
-        #         else:
-        #             param_group["lr"] = lr
-        #     print(f'Updating learning rate to {lr:.7f}')
-        #     return lr
-
-        # train_times = [] # For computational cost analysis
         for epoch in range(self.args.train_epochs):
             iter_count = 0
             train_loss = []
-            # train_time = 0 # For computational cost analysis
-
             self.model.train()
             epoch_time = time.time()
+
             for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
                 iter_count += 1
                 model_optim.zero_grad()
+
                 batch_x = batch_x.float().to(self.device)
-
                 batch_y = batch_y.float().to(self.device)
-                batch_x_mark = batch_x_mark.float().to(self.device)
-                batch_y_mark = batch_y_mark.float().to(self.device)
 
-                # decoder input
-                dec_inp = torch.zeros_like(batch_y[:, -self.args.pred_len:, :]).float()
-                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
-
-                # encoder - decoder
-                # temp = time.time() # For computational cost analysis
                 outputs = self.model(batch_x)
-                # train_time += time.time() - temp # For computational cost analysis
                 f_dim = -1 if self.args.features == 'MS' else 0
                 outputs = outputs[:, -self.args.pred_len:, f_dim:]
-                batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
+                batch_y = batch_y[:, -self.args.pred_len:, f_dim:]
 
-                # CARD loss with weight decay
-                # self.ratio = np.array([max(1/np.sqrt(i+1),0.0) for i in range(self.args.pred_len)])
+                # Arctangent-scaled MAE loss
+                ratio = self._get_ratio()
+                loss = mae_criterion(outputs * ratio, batch_y * ratio)
 
-                # Arctangent loss with weight decay
-                self.ratio = np.array([-1 * math.atan(i+1) + math.pi/4 + 1 for i in range(self.args.pred_len)])
-                self.ratio = torch.tensor(self.ratio).unsqueeze(-1).to('cuda')
-
-                outputs = outputs * self.ratio
-                batch_y = batch_y * self.ratio
-
-                loss = mae_criterion(outputs, batch_y)
-
-                # loss = criterion(outputs, batch_y) # For MSE criterion
+                # EMA alpha regularization
+                get_reg = self._get_model_attr('get_ema_reg_loss')
+                if get_reg is not None:
+                    loss = loss + get_reg()
 
                 train_loss.append(loss.item())
 
                 if (i + 1) % 100 == 0:
-                    print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
+                    print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(
+                        i + 1, epoch + 1, loss.item()))
                     speed = (time.time() - time_now) / iter_count
                     left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
                     print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
@@ -188,41 +142,46 @@ class Exp_Main(Exp_Basic):
                 loss.backward()
                 model_optim.step()
 
-            # train_times.append(train_time/len(train_loader)) # For computational cost analysis
             print("Epoch: {} cost time: {}".format(epoch + 1, time.time() - epoch_time))
             train_loss = np.average(train_loss)
-            # vali_loss = self.vali(vali_data, vali_loader, criterion) # For MSE criterion
-            # test_loss = self.vali(test_data, test_loader, criterion) # For MSE criterion
             vali_loss = self.vali(vali_data, vali_loader, mae_criterion, is_test=False)
             test_loss = self.vali(test_data, test_loader, mse_criterion)
 
+            # Log learned alpha values
+            get_alpha = self._get_model_attr('get_alpha_values')
+            if get_alpha is not None:
+                alphas = get_alpha()
+                alpha_history.append(alphas.copy())
+                alpha_str = ', '.join([f'{a:.4f}' for a in alphas[:5]])
+                if len(alphas) > 5:
+                    alpha_str += f', ... ({len(alphas)} total)'
+                print(f"  Learned alphas: [{alpha_str}]")
+
             print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Test Loss: {4:.7f}".format(
                 epoch + 1, train_steps, train_loss, vali_loss, test_loss))
-            early_stopping(vali_loss, self.model, path)
 
+            early_stopping(vali_loss, self.model, path)
             if early_stopping.early_stop:
                 print("Early stopping")
                 break
 
             adjust_learning_rate(model_optim, epoch + 1, self.args)
-            # adjust_learning_rate_new(model_optim, epoch + 1, self.args)
 
-            # print('Alpha:', self.model.decomp.ma.alpha) # Print the learned alpha
-            # print('Beta:', self.model.decomp.ma.beta)   # Print the learned beta
+        if alpha_history:
+            np.save(os.path.join(path, 'alpha_history.npy'), np.array(alpha_history))
 
-        # print("Training time: {}".format(np.sum(train_times)/len(train_times))) # For computational cost analysis
         best_model_path = path + '/' + 'checkpoint.pth'
         self.model.load_state_dict(torch.load(best_model_path))
         os.remove(best_model_path)
-
         return self.model
 
     def test(self, setting, test=0):
         test_data, test_loader = self._get_data(flag='test')
-        
+
         if test:
             print('loading model')
-            self.model.load_state_dict(torch.load(os.path.join('./checkpoints/' + setting, 'checkpoint.pth')))
+            self.model.load_state_dict(
+                torch.load(os.path.join('./checkpoints/' + setting, 'checkpoint.pth')))
 
         preds = []
         trues = []
@@ -230,67 +189,48 @@ class Exp_Main(Exp_Basic):
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
-        # test_time = 0 # For computational cost analysis
         self.model.eval()
         with torch.no_grad():
             for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(test_loader):
                 batch_x = batch_x.float().to(self.device)
                 batch_y = batch_y.float().to(self.device)
 
-                batch_x_mark = batch_x_mark.float().to(self.device)
-                batch_y_mark = batch_y_mark.float().to(self.device)
-
-                # decoder input
-                dec_inp = torch.zeros_like(batch_y[:, -self.args.pred_len:, :]).float()
-                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
-                # encoder - decoder
-                # temp = time.time() # For computational cost analysis
                 outputs = self.model(batch_x)
-                # test_time += time.time() - temp # For computational cost analysis
-
                 f_dim = -1 if self.args.features == 'MS' else 0
                 outputs = outputs[:, -self.args.pred_len:, f_dim:]
-                batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
+                batch_y = batch_y[:, -self.args.pred_len:, f_dim:]
+
                 outputs = outputs.detach().cpu().numpy()
                 batch_y = batch_y.detach().cpu().numpy()
 
-                pred = outputs  # outputs.detach().cpu().numpy()  # .squeeze()
-                true = batch_y  # batch_y.detach().cpu().numpy()  # .squeeze()
-
-                preds.append(pred)
-                trues.append(true)
+                preds.append(outputs)
+                trues.append(batch_y)
 
                 if i % 20 == 0:
-                    input = batch_x.detach().cpu().numpy()
-                    gt = np.concatenate((input[0, :, -1], true[0, :, -1]), axis=0)
-                    pd = np.concatenate((input[0, :, -1], pred[0, :, -1]), axis=0)
+                    inp = batch_x.detach().cpu().numpy()
+                    gt = np.concatenate((inp[0, :, -1], batch_y[0, :, -1]), axis=0)
+                    pd = np.concatenate((inp[0, :, -1], outputs[0, :, -1]), axis=0)
                     visual(gt, pd, os.path.join(folder_path, str(i) + '.pdf'))
-            
-        # print("Inference time: {}".format(test_time/len(test_loader))) # For computational cost analysis
+
         preds = np.array(preds)
         trues = np.array(trues)
-        # preds = np.concatenate(preds, axis=0) # without the "drop-last" trick
-        # trues = np.concatenate(trues, axis=0) # without the "drop-last" trick
-
         preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
         trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
 
-        # # result save
-        # folder_path = './results/' + setting + '/'
-        # if not os.path.exists(folder_path):
-        #     os.makedirs(folder_path)
-
         mae, mse = metric(preds, trues)
         print('mse:{}, mae:{}'.format(mse, mae))
+
         f = open("result.txt", 'a')
         f.write(setting + "  \n")
         f.write('mse:{}, mae:{}'.format(mse, mae))
-        f.write('\n')
-        f.write('\n')
+        f.write('\n\n')
         f.close()
 
-        # np.save(folder_path + 'metrics.npy', np.array([mae, mse, rmse, mape, mspe,rse, corr]))
-        # np.save(folder_path + 'pred.npy', preds)
-        # np.save(folder_path + 'true.npy', trues)
-        # np.save(folder_path + 'x.npy', inputx)
+        # Save final alphas
+        get_alpha = self._get_model_attr('get_alpha_values')
+        if get_alpha is not None:
+            alphas = get_alpha()
+            print(f"Final learned alphas: {alphas}")
+            np.save(os.path.join(folder_path, 'final_alphas.npy'), alphas)
+
         return
